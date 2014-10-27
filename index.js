f40ab463ed180d0cfd2b05d3083c9382d6000d2b
Colin Jack <colin.jack@gmail.com>
2014-10-27 17:06:14 +0000

    Bit more validation and associated tests.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var _u = require('underscore');
 var winston = require('winston');
 var Namespace = require('./Namespace');
+var fs = require('fs');
 
 module.exports = (function() {
     var createSync = function(associatedDir, name) {
-        try
-        {
+        try {
+            validateArguments(associatedDir);
+
             var namespace = new Namespace(name);
             
             if (associatedDir.lastIndexOf("/") < associatedDir.length - 1) {
@@ -26,6 +28,19 @@ module.exports = (function() {
             throw ex;
         }
     };
+    
+    var validateArguments = function (directoryPath) {
+        var directoryPathIsNotString = typeof directoryPath != 'string' && !(directoryPath instanceof String);
+
+        if (directoryPathIsNotString || directoryPath.length == 0) {
+            throw new ReferenceError("You must provide the directory path to scan.");
+        }
+        
+        if (!fs.existsSync(directoryPath)) {
+            var message = "The specified directory does not exist " + directoryPath + ".";
+            throw new ReferenceError(message);
+        }
+    }
 
     return {
         createSync: createSync
---
+++ spec/createSync_argument_specs.js
@@ -0,0 +1,48 @@
+﻿var assert = require('chai').assert;
+var RegistersResources = lib.require('RegistersResources');
+
+describe('RegistersResources', function () {
+    describe("invalid directory path provided", function () {
+        var validFakeExpressInstance = { use: function () { } };
+        
+        describe("when given null directory", function () {
+            it("should raise an exception", function () {
+                assert.throws(function () {
+                    RegistersResources.registerAllInDirectory(null, validFakeExpressInstance)
+                }, /You must provide the directory path to scan./)
+            })
+        });
+        
+        describe("when given undefined directory", function () {
+            it("should raise an exception", function () {
+                assert.throws(function () {
+                    RegistersResources.registerAllInDirectory(undefined, validFakeExpressInstance)
+                }, /You must provide the directory path to scan./)
+            })
+        });
+        
+        describe("when given non-string directory", function () {
+            it("should raise an exception", function () {
+                assert.throws(function () {
+                    RegistersResources.registerAllInDirectory(5, validFakeExpressInstance)
+                }, /You must provide the directory path to scan./)
+            })
+        });
+        
+        describe("when given empty directory", function () {
+            it("should raise an exception", function () {
+                assert.throws(function () {
+                    RegistersResources.registerAllInDirectory("", validFakeExpressInstance)
+                }, /You must provide the directory path to scan./)
+            })
+        });
+        
+        describe("when given directory path is to non-existent directory", function () {
+            it("should raise an exception", function () {
+                assert.throws(function () {
+                    RegistersResources.registerAllInDirectory("c:\\foo\\bar\\this\\is\\not\\going\\to\\exist", validFakeExpressInstance)
+                }, "The specified directory does not exist c:\\foo\\bar\\this\\is\\not\\going\\to\\exist")
+            })
+        });
+    });
+});
