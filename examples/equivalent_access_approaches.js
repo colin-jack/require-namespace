912e9f54b950b7e09fac3fff98115ac2a782f7c2
Colin Jack <colin.jack@gmail.com>
2014-12-07 19:42:49 +0000

    Removed spurious comment.

---
+++ examples/equivalent_access_approaches.js
@@ -0,0 +1,9 @@
+﻿// just shows the different ways of accessing things
+var assert = require('chai').assert;
+
+var namespaces = require('./../index');
+var simpleNamespace = namespaces.createSync(__dirname + "/simple", "simple");
+
+assert.equal(namespaces.simple, simpleNamespace);
+assert.equal(simpleNamespace.require("foo"), simpleNamespace.foo);
+
